65aded038fe9097c35a06dfc79573d0169f2b4fc
juanguren <juan_fe_7@live.com>
2021-03-27 12:05:42 -0500

    Modularized Schema

---
+++ JEST/src/database/Users/users.schema.ts
@@ -0,0 +1,19 @@
+
+import { Schema } from "mongoose";
+
+const UserSchema : Schema = new Schema({
+    id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    age: {type: String, required: true },
+    income: { type: Number, required: true },
+    admin: { type: Boolean, required: true },
+    professionType: 
+    { 
+        type: String,
+        enum: [ 'STUDENT', 'PROFESSIONAL', 'RETIRED' ],
+        required: true,
+        default: 'STUDENT'
+    }
+});
+
+export default UserSchema;
