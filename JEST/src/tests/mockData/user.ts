9985c51c02412212286707f47045e0ff3b5e6d09
juanguren <juan_fe_7@live.com>
2021-04-02 23:12:12 -0500

    Imported and applied faker for easier testing

--- JEST/src/tests/mockData/user.ts
+++ JEST/src/tests/mockData/user.ts
@@ -1,9 +1,13 @@
 
+import * as fk from 'faker';
+
+const fkAge = (fk.datatype.number().toString()).substring(0,2);
+
 export const mockUser = {
-    "code": "2567",
-    "name": "Juan",
-    "age": "25",
-    "income": 87000,
-    "admin": true,
-    "professionType": "PROFESSIONAL"
+    "code": fk.datatype.uuid(),
+    "name": fk.name.findName(),
+    "age": fkAge,
+    "income": fk.datatype.number()*10,
+    "admin": false,
+    "professionType": Number(fkAge) > 65 ? 'RETIRED' : 'PROFESSIONAL'
 }
