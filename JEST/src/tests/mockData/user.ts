704515035484fc04c6b937aaa55cf1a2efef742f
juanguren <juan_fe_7@live.com>
2021-04-22 14:44:05 -0500

    Included a mock-like object

--- JEST/src/tests/mockData/user.ts
+++ JEST/src/tests/mockData/user.ts
@@ -33,6 +33,17 @@ export const mockResponse
     }
 ];
 
+export const mockResponseById = {
+    "professionType": "PROFESSIONAL",
+    "_id": "6060fecd4d26aa40ec1df90d",
+    "code": "KJ8",
+    "name": "Juan",
+    "age": "25",
+    "income": 95300,
+    "admin": true,
+    "__v": 0
+}
+
 export const faultyUser = {
     "code": fk.datatype.uuid(),
     "name": fk.name.findName(),
