dead7f028e2760aead38f6dae8f03217147a5d90
Juan Felipe <juan_fe_7@live.com>
2021-07-11 19:40:46 -0500

    falsy_bouncer

---
+++ Algorithms/falsy_bouncer.ts
@@ -0,0 +1,8 @@
+const bouncer = (array: Array<any>): Array<string | number> => {
+  const noFalsy = array.filter((element: any) => {
+    if (element) return element;
+  });
+  return noFalsy;
+};
+
+bouncer([7, "ate", "", false, 9]);
