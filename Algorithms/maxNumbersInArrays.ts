d706a6d9900ed37f45b6744c997fc9945208081e
Juan Felipe <juan_fe_7@live.com>
2022-03-31 20:52:28 -0500

    Optimized the maxNumbersInArray solution

--- Algorithms/maxNumbersInArrays.ts
+++ Algorithms/maxNumbersInArrays.ts
@@ -1,20 +1,16 @@
-const largestOfFour = (numbers: Array<Array<number>>): Array<number> => {
-  const maxValuesArray: Array<number> = [];
-
-  for (let i = 0; i < numbers.length; i++) {
-    const maxPerArray = numbers[i].reduce((accumulator, value): number => {
-      return Math.max(accumulator, value);
-    });
-
-    maxValuesArray.push(maxPerArray);
+function largestOfFour(values: Array<any>) {
+  const largestList = [];
+  for (let group of values) {
+    largestList.push(Math.max(...group));
   }
+  return largestList;
+}
 
-  return maxValuesArray;
-};
-
-largestOfFour([
+const values = [
   [4, 5, 1, 3],
   [13, 27, 18, 26],
   [32, 35, 37, 39],
   [1000, 1001, 857, 1],
-]);
+];
+
+console.log(largestOfFour(values));
