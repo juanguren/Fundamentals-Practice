2c9959f444aa4d0545595146aa52d6177a69a30c
Juan Felipe <juan_fe_7@live.com>
2022-03-31 21:17:35 -0500

    Included the confirmTheEnding puzzle

---
+++ Algorithms/confirmTheEnding.ts
@@ -0,0 +1,15 @@
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+function confirmEnding(word: string, target: string): boolean {
+  const stringLength = word.length;
+  const targetLength = target.length;
+  const lastCharacter = word.substring(
+    stringLength,
+    stringLength - targetLength
+  );
+
+  if (lastCharacter == target) return true;
+  return false;
+}
+
+confirmEnding("Bastian", "n");
