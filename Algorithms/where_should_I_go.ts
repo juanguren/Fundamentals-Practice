3871660726b550388df2a79a8c703492a37b1203
Juan Felipe <juan_fe_7@live.com>
2022-04-02 19:10:22 -0500

    Optimized the calculateMinIndex

--- Algorithms/where_should_I_go.ts
+++ Algorithms/where_should_I_go.ts
@@ -3,12 +3,11 @@
  * once it has been sorted. The returned value should be a number.
  */
 
-const calculateIndex = (stack: Array<number>, value: number): number => {
-  stack.push(value);
-  const sortedArray = stack.sort((a, b) => {
-    if (a < b) return -1;
-  });
-  return sortedArray.indexOf(value);
-};
+function calculateMinIndex(values, num) {
+  values.push(num);
+  values.sort((a, b) => a - b);
 
-calculateIndex([10, 5, 65, 2.5, 87, 6, 60, 65, 1, 799, 100], 20.5);
+  return values.indexOf(num);
+}
+
+calculateMinIndex([10, 5, 65, 2.5, 87, 6, 60, 65, 1, 799, 100], 20.5);
