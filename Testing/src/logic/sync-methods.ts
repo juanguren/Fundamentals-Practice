17cb09d372e561d4f79756255d1cef1b7c97db9a
Juan Felipe <juan_fe_7@live.com>
2022-04-15 10:31:34 -0500

    Included extra tests, increasing complexity

--- Testing/src/logic/sync-methods.ts
+++ Testing/src/logic/sync-methods.ts
@@ -1,3 +1,5 @@
+const faker = require("faker");
+
 export const multiplyByX = (array: any[], multiplier: number): any => {
   const multiply = (value: number) => value * multiplier;
   const isNumber = (value: any): value is number => typeof value === "number";
@@ -7,4 +9,32 @@ export const multiplyByX = (array: any[], multiplier: number): any => {
   );
 
   return multiplied.filter(isNumber);
+};
+
+export const largestOfFour = (values: any[]) => {
+  const largestList = [];
+  for (let group of values) {
+    largestList.push(Math.max(...group));
+  }
+  return largestList;
+};
+
+export interface IUser {
+  name: string;
+  age: number;
+  ssn: string;
+  drinkingStatus: boolean;
+}
+
+export const getDrinkingStatus = (age: number) => age >= 18;
+
+export const getUser = (): IUser => {
+  const age = faker.datatype.number({ min: 13, max: 80 });
+
+  return {
+    name: faker.name.firstName(),
+    age,
+    ssn: faker.datatype.uuid(),
+    drinkingStatus: getDrinkingStatus(age),
+  };
 };
--- Testing/src/tests/sync-methods.spec.ts
+++ Testing/src/tests/sync-methods.spec.ts
@@ -1,24 +1,51 @@
-import { multiplyByX } from "../logic/sync-methods";
+import { getUser, largestOfFour, multiplyByX } from "../logic/sync-methods";
+const faker = require("faker");
 
 describe("Test some random sync methods", () => {
-  // Create a method that multiplies every element (number) in an array by X value
-  it("Should return an array of numbers multiplied by X number", () => {
-    const array = [1, 2, 3, 4, 5];
-    const multiplier = 2;
-    const expected = [2, 4, 6, 8, 10];
+  describe("method that multiplies every element (number) in an array by X value", () => {
+    it("Should return an array of numbers multiplied by X number", () => {
+      const array = [1, 2, 3, 4, 5];
+      const multiplier = 2;
+      const expected = [2, 4, 6, 8, 10];
 
-    expect(multiplyByX(array, multiplier)).toEqual(expected);
+      expect(multiplyByX(array, multiplier)).toEqual(expected);
+    });
+
+    it("Should skip an element if it doesn't fulfill the multiplier's condition", () => {
+      const array = [1, 2, 3, true, ""];
+      const multiplier = 2;
+      const expected = [2, 4, 6];
+      const expectedArrayLength = expected.length;
+
+      const response = multiplyByX(array, multiplier);
+
+      expect(response).toEqual(expected);
+      expect(response.length).toEqual(expectedArrayLength);
+    });
   });
+  describe("Method that calculates the biggest numbers in X ammount of arrays and then return those as a flat array", () => {
+    it("Should return an array of the biggest numbers in each array", () => {
+      const values = [
+        [5, 15, 80, 3],
+        [5, 10, 3, 1],
+        [1, 2, 3, 4, 5],
+      ];
+      const expected = [80, 10, 5];
 
-  it("Should skip an element if it doesn't fulfill the multiplier's condition", () => {
-    const array = [1, 2, 3, true, ""];
-    const multiplier = 2;
-    const expected = [2, 4, 6];
-    const expectedArrayLength = expected.length;
+      const response = largestOfFour(values);
 
-    const response = multiplyByX(array, multiplier);
+      expect(response).toEqual(expect.any(Array));
+      expect(response.length).toEqual(expected.length);
+      expect(response).toEqual(expected);
+    });
+  });
+  describe("Method that calls library and a native node module", () => {
+    it("Should return an object composed of 4 key/values", () => {
+      const response = getUser();
 
-    expect(response).toEqual(expected);
-    expect(response.length).toEqual(expectedArrayLength);
+      expect(response).toEqual(expect.any(Object));
+      expect(response).toHaveProperty("drinkingStatus");
+      expect(response.drinkingStatus).toStrictEqual(expect.any(Boolean));
+    });
   });
 });
