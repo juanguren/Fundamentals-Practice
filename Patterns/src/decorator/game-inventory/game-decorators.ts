cae4d724ad0516c5b834291ad6f8abbb0dd3752d
Juan Felipe <juan_fe_7@live.com>
2022-04-17 12:57:40 -0500

    Updated packages and housekeep

--- Patterns/src/decorator/game-inventory/game-decorators.ts
+++ Patterns/src/decorator/game-inventory/game-decorators.ts
@@ -1,13 +1,13 @@
 import Hero from "./Hero";
 import { itemType } from "./types";
 
-const addItems = (hero: Hero, element: any[], item: itemType): Hero => {
+const addItems = (hero: Hero, elements: any[], item: itemType): Hero => {
   const itemSelector = {
     food: hero.inventory.food,
     weapons: hero.inventory.weapons,
   };
 
-  itemSelector[item].push(...element);
+  itemSelector[item].push(...elements);
   return hero;
 };
 
