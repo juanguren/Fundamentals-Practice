92139412a81198cfeaccec995743979e39f5ebb6
Juan Felipe Aranguren <juan.aranguren@globant.com>
2022-04-20 17:48:38 -0500

    Scaled the addItems() decorator in order to handle item lists

--- Patterns/index.ts
+++ Patterns/index.ts
@@ -1,11 +1,22 @@
 import Hero from "./src/decorator/game-inventory/Hero";
 import Decorator from "./src/decorator/game-inventory/game-decorators";
-import { itemType, Weapon } from "./src/decorator/game-inventory/types";
+import { Food, itemType, Weapon } from "./src/decorator/game-inventory/types";
 
 const hero = new Hero("Parcival");
 console.log(hero);
 
 const newWeapon: Weapon = { name: "Orb of ossobox", active: true, damage: 9 };
+const newWeaponTwo: Weapon = {
+  name: "Excalibur",
+  active: true,
+  damage: 10,
+};
+const newDish: Food = { name: "Ceviche Peruano", points: 15 };
 // itemType FOOD with key/values of weapon gets added anyway. How to type check in the decorator?
-const heroWithWeapons = Decorator.addItems(hero, [newWeapon], itemType.WEAPON);
+const heroWithWeapons = Decorator.addItems(hero, [
+  newWeapon,
+  newDish,
+  newWeaponTwo,
+]);
+
 console.log(heroWithWeapons.getInventory());
--- Patterns/src/decorator/game-inventory/game-decorators.ts
+++ Patterns/src/decorator/game-inventory/game-decorators.ts
@@ -1,13 +1,22 @@
 import Hero from "./Hero";
-import { itemType } from "./types";
+import { Food, itemType, Weapon } from "./types";
 
-const addItems = (hero: Hero, elements: any[], item: itemType): Hero => {
+const addItems = (hero: Hero, elements: (Weapon | Food)[]): Hero => {
   const itemSelector = {
     food: hero.inventory.food,
     weapons: hero.inventory.weapons,
   };
 
-  itemSelector[item].push(...elements);
+  elements.forEach((item) => {
+    if ((item as Weapon).damage) {
+      const newWeapon = item as Weapon;
+      hero.inventory.weapons.push(newWeapon);
+    } else {
+      const newDish = item as Food;
+      hero.inventory.food.push(newDish);
+    }
+  });
+
   return hero;
 };
 
