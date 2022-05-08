4efb6671588aee3662075c30b8025562f48616cd
Juan Felipe <juan_fe_7@live.com>
2022-05-08 18:41:42 -0500

    Typed the Character class

--- OOP/game/src/classes/characters/Character.ts
+++ OOP/game/src/classes/characters/Character.ts
@@ -1,9 +1,15 @@
-export class Character {
-  constructor(
-    public name: string,
-    public health: number = 100,
-    public level: number = 1,
-  ) {
-    this.name = name;
+import { ICharacter } from '../../interfaces/character';
+import { initialInventory } from '../items/item-utils';
+import { IInventory } from '../items/items-interface';
+
+export abstract class Character implements ICharacter {
+  name: string;
+  health: number = 100;
+  level: number = 1;
+  private isAlive: boolean = true;
+  inventory: IInventory = initialInventory;
+  constructor(_name: string) {
+    this.name = _name;
   }
+  abstract attack(target: ICharacter | any): number;
 }
