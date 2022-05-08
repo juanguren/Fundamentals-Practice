12745cf86d56ef8aa4d1640fab1dc6907c5e6e05
Juan Felipe <juan_fe_7@live.com>
2022-05-08 18:45:20 -0500

    Tried out: - Character creation (element) - One character attacking another.

--- OOP/game/start.ts
+++ OOP/game/start.ts
@@ -1,1 +1,35 @@
 // https://betterprogramming.pub/understand-object-oriented-programming-with-typescript-c4ff8afa40d
+
+import { Fenix } from './src/classes/characters/fenix';
+import { Golem } from './src/classes/characters/golem';
+
+const fenix = new Fenix('Fawkes');
+
+const earthling = new Golem('Earthling');
+
+console.log(fenix.attack(earthling)); // 97.5
+
+console.log(fenix);
+/**
+ *Fenix {
+  health: 100,
+  level: 1,
+  isAlive: true,
+  inventory: { potion: [], weapon: [ [Object] ], armor: [ [Object] ] },
+  name: 'Fenix',
+  element: Element {
+    class: 'fire',
+    elementInstance: Fire { attacks: [Object] },
+    attacks: { scorchingSun: [Function: scorchingSun] }
+  }
+}
+ */
+
+console.log(fenix.inventory);
+/**
+ {
+  potion: [],
+  weapon: [ { id: 1, name: 'Sword', damage: 2, element: 'fire' } ],
+  armor: [ { id: 1, name: 'Leather Armor', defense: 2, element: 'fire' } ]
+}
+ */
