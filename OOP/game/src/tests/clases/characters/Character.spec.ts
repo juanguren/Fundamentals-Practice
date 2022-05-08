8be038e1e425533621adf3dce37df3208e2be6fb
Juan Felipe <juan_fe_7@live.com>
2022-05-08 18:44:00 -0500

    Skipped abstract class tests

--- OOP/game/src/tests/clases/characters/Character.spec.ts
+++ OOP/game/src/tests/clases/characters/Character.spec.ts
@@ -1,9 +1,9 @@
 import { Character } from '../../../classes/characters/Character';
 
 describe('Character', () => {
-  it('should create a Character instance', () => {
-    const character = new Character('Eamon');
+  it.skip('should create a Character instance', () => {
+    /*const character = new Character('Eamon');
 
-    expect(character).toBeInstanceOf(Character);
+    expect(character).toBeInstanceOf(Character);*/
   });
 });
