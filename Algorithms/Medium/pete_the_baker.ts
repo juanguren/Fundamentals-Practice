bfb6d35485a716ad6240eea00f29bd945120e92c
Juan Felipe Aranguren <juan_fe_7@live.com>
2022-07-16 18:38:03 -0500

    Organized algorithms by level

--- Algorithms/Medium/pete_the_baker.ts
+++ Algorithms/Medium/pete_the_baker.ts
@@ -5,10 +5,10 @@
  * Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number
    of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200).
    Ingredients that are not present in the objects, can be considered as 0.
-   
+
  * @param recipe - The recipe of the cake
  * @param available - The available ingredients
- * @returns The minimum number of cakes that can be made
+ * @returns The maximum number of cakes that can be made
  */
 
 function cakes(
