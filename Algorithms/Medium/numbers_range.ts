13314bb852cc16ca0ce2c0118704abf02946e667
Juan Felipe Aranguren <juan_fe_7@live.com>
2022-08-01 20:52:51 -0500

    Included algorithm description

--- Algorithms/Medium/numbers_range.ts
+++ Algorithms/Medium/numbers_range.ts
@@ -1,3 +1,10 @@
+
+/**
+ * We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+ * The lowest number will not always come first.
+ * @param values A two-number only array.
+ * @returns The sum of the two numbers and all the numbers between them.
+ */
 function sumAll(values: [number, number]): number {
   const ordered = values.sort((a, b) => (a < b ? -1 : 1));
   let sum = ordered[0] + ordered[1];
